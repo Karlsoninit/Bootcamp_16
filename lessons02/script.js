235ef8e796081a7d1a06dcbfc46e638af290926c
NoretsMaksim <34502848+Karlsoninit@users.noreply.github.com>
2019-11-07 17:10:27 +0200

    made calculator use callback function and done teach function

--- lessons02/script.js
+++ lessons02/script.js
@@ -113,10 +113,10 @@
 
 //-- function declaration
 
-function add(a, b) {
-  let result = a * b;
-  return result;
-}
+// function add(a, b) {
+//   let result = a * b;
+//   return result;
+// }
 
 // const result = add(21, 23, 43);
 // console.log(result);
@@ -133,16 +133,16 @@ function add(a, b) {
 // fn(1, 2, 34, 5, 6, 7, 7, 34, 43, 6, 78, 89, 1);
 
 //with ...rest
-const shortFn = (arr, ...rest) => {
-  console.log(arr);
-  console.log(rest);
-  let con = arr.concat(rest);
-  console.log(con);
-};
+// const shortFn = (arr, ...rest) => {
+//   console.log(arr);
+//   console.log(rest);
+//   let con = arr.concat(rest);
+//   console.log(con);
+// };
 
-const arr = [4, 5, 6];
+// const arr = [4, 5, 6];
 
-shortFn(arr, 3, 3, 35, 5, 6);
+// shortFn(arr, 3, 3, 35, 5, 6);
 
 // const shortFn = (a, b) => {
 //   let res = a * b;
@@ -150,3 +150,149 @@ shortFn(arr, 3, 3, 35, 5, 6);
 
 // const result = shortFn(4, 2);
 // console.log(result);
+
+//----------- part 2 ------- function
+
+const arr = [1, 2, 34, 5, 6, 7, 7, 34, 43, 6, 78, 89, 1];
+
+const fn = function(arr) {
+  const result = someUse(arr);
+  const returnMult = mult(result);
+  message(returnMult);
+};
+
+const someUse = function(par) {
+  const newArr = [];
+  for (let elem of par) {
+    if (elem > 20) {
+      newArr.push(elem);
+    }
+  }
+
+  return newArr;
+};
+
+const message = function(res) {
+  console.log(res);
+};
+
+const mult = function(mass) {
+  const arr = [];
+  for (let elem of mass) {
+    let res = elem * elem;
+    arr.push(res);
+  }
+  return arr;
+};
+
+// fn(arr);
+
+// const choose = prompt("choose ...");
+
+const show = function(arr, choose) {
+  if (choose === "multiply") {
+    console.log(mult(arr));
+  } else if (choose === "more") {
+    console.log(someUse(arr));
+  } else {
+    message("some text");
+  }
+};
+
+// show(arr, choose);
+
+//------------ task --------
+
+/*
+  Напиши функцию filterFromArray(arr), которая 
+  объявляет 1 параметр, исходный массив arr.
+  
+  При вызове функции, первым аргументом всегда будет массив чисел,
+  за которым следуют один или несколько аргументов, тоже чисел. 
+  
+  Функция возвращает новый массив, который содержит только 
+  те элементы исходного массива, которые не совпадают 
+  ни с одним из числовых аргументов.
+*/
+
+const filterFromArray = function(first, second, num) {
+  // console.log(arr.includes());
+
+  // --- use only spred operator ...spred
+  const newArr = [...first, ...second];
+  // newArr.push("dsfd", "fdfd");
+  // console.log(newArr);
+  const mass = [];
+
+  for (let elem of newArr) {
+    if (!mass.includes(elem)) {
+      mass.push(elem);
+    }
+  }
+  return mass;
+};
+
+// Вызовы функции для проверки
+console.log(filterFromArray([1, 2, 3, 4, 5, 2], [4, 4, 45, 68, 9, 9, 5]));
+// [1, 3, 5]
+
+// console.log(filterFromArray([12, 4, 3, 8, 17], 3, 29, 18, 4)); // [12, 8, 17]
+
+// console.log(window);
+
+// const unique = function(callback) {
+//   console.log(fn1);
+//   // callback();
+// };
+
+// unique(() => {
+//   console.log("work");
+// });
+
+//------- create aesy calculator ---------
+
+const numberFirst = Number(prompt("enter num ..."));
+const calculations = prompt("enter num ...");
+const numberSecond = Number(prompt("enter num ..."));
+
+const calculator = (plus, minus, multi) => {
+  // Math.floor(Math.random() > 0.5) ? one() : two();
+  if (!isNaN(calculations) === "+") {
+    return plus(numberFirst, numberSecond);
+  } else if (!isNaN(calculations) === "-") {
+    return minus(numberFirst, numberSecond);
+  } else if (!isNaN(calculations) === "*") {
+    return multi(numberFirst, numberSecond);
+  } else {
+    console.error("eeeeeer");
+  }
+};
+
+const plus = (parOne, paramTwo) => {
+  return parOne + paramTwo;
+};
+
+const result = calculator(
+  plus,
+  (parOne, paramTwo) => {
+    return parOne - paramTwo;
+  },
+  (parOne, paramTwo) => {
+    return parOne * paramTwo;
+  }
+);
+
+console.log(result);
+
+// проверка на ввод числа
+
+// const someFn = (a, b) => {
+//   // if (typeof a === "string" || typeof b === "string") {
+//   //   return;
+//   // }
+//   console.log(a + b);
+// };
+
+// // someFn(3, "5");
+
+// someFn(3, "5");
