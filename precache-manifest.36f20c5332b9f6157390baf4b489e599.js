4895ab9d6c39daa2e6c6766842e71ce4a979f8a2
NoretsMaksim <34502848+Karlsoninit@users.noreply.github.com>
2019-12-18 12:00:53 +0200

    Updates

---
+++ precache-manifest.36f20c5332b9f6157390baf4b489e599.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a115542d3c70ba81f8d76f65a26bc94a",
+    "url": "/Bootcamp_16/./index.html"
+  },
+  {
+    "revision": "f5b126bc2cf46bc40744",
+    "url": "/Bootcamp_16/./static/css/main.00f978f0.chunk.css"
+  },
+  {
+    "revision": "4f537d8aab55335e95ab",
+    "url": "/Bootcamp_16/./static/js/2.268f91c2.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Bootcamp_16/./static/js/2.268f91c2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5b126bc2cf46bc40744",
+    "url": "/Bootcamp_16/./static/js/main.2159464d.chunk.js"
+  },
+  {
+    "revision": "311fc726f2586f3c68ab",
+    "url": "/Bootcamp_16/./static/js/runtime-main.f1a525c6.js"
+  }
+]);
